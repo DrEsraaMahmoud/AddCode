ce20174bde9cd30aedb196db3b096cc0dd1a4fcc
DrEsraaMahmoud <E17521rs>
2023-03-22 03:05:36 +0300

    First release

---
+++ Add.java
@@ -0,0 +1,26 @@
+package project1;
+
+
+public class Add {
+	
+	   public static void main (String[] args) {
+	   
+	    int a,b,ans;
+	    /**
+	     * @param a first number
+	    */
+	    
+	    a = 4;
+	    
+	    /**
+	    * @param b second number 
+	    */
+	    
+	    b = 5;
+	    ans = a+b;
+	    System.out.println("Addition of 2 numbers is:" + ans);
+
+	    }
+	   
+} 
+
